49aedd171eeb8b7c38fa4e7479886ef13f02c195
blackdynamo <d16345401@gmail.com>
2015-01-18 17:04:13 -0700

    Modifying to support bower.

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 /**
  * Basic Pipeline
- * Copyright (c) 2013. Donnovan Lewis
+ * Copyright (c) 2013-2015. Donnovan Lewis
  * MIT License
  */
 
-function Pipeline() {
-    this._stack = [];
-}
-
-Pipeline.prototype.use = function (step) {
-    this._stack.push(step);
-    return this;
-};
-
-Pipeline.prototype.execute = function (context, cb) {
-    var me = this,
-        index = 0;
-
-    function next(err) {
-        var step = me._stack[index++];
-
-        if (!step || err) {
-            cb(err, context);
-            return;
-        }
-
-        try {
-            step(context, next);
-        }
-        catch (e) {
-            cb(e, context);
-        }
+(function (global, module) {
+    function Pipeline() {
+        this._stack = [];
     }
 
-    next();
-};
+    Pipeline.prototype.use = function (step) {
+        this._stack.push(step);
+        return this;
+    };
 
-module.exports = Pipeline;
+    Pipeline.prototype.execute = function (context, cb) {
+        var me = this,
+            index = 0;
+
+        cb = cb || function () {
+        };
+
+        function next(err) {
+            var step = me._stack[index++];
+
+            if (!step || err) {
+                cb(err, context);
+                return;
+            }
+
+            try {
+                step(context, next);
+            }
+            catch (e) {
+                cb(e, context);
+            }
+        }
+
+        next();
+    };
+
+    //Define an AMD module if AMD is present
+    if (typeof define === "function" && define.amd) {
+        define("BasicPipeline", [], function () {
+            return Pipeline;
+        });
+    }
+
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = Pipeline;
+    }
+    else {
+        global.BasicPipeline = Pipeline;
+    }
+}(typeof window !== "undefined" ? window : this, typeof module !== "undefined" ? module : undefined));
